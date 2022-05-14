8fcc969badfc2f25927ae0024c9588c0ad56b39b
McManOfTheLand <forrestchew@gmail.com>
2022-05-13 17:18:11 -0700

    sets marketplace address in factory smart contract

---
+++ scripts/factory-scripts/set-marketplace-address.js
@@ -0,0 +1,14 @@
+const { getCardFactoryInstance, marketplaceAddress } = require('../utils.js');
+
+const setMarketplaceAddress = async () => {
+  const cardFactory = await getCardFactoryInstance();
+  cardFactory.setMarketplaceAddress(marketplaceAddress);
+  console.log('Marketplace address set in factory to:', marketplaceAddress);
+};
+
+setMarketplaceAddress()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
