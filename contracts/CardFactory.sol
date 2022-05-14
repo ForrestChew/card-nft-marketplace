d00496405ea7a3e7842b51d20a4049d12072a252
McManOfTheLand <forrestchew@gmail.com>
2022-05-13 17:16:21 -0700

    Added chainlink VRF capabilities

--- contracts/CardFactory.sol
+++ contracts/CardFactory.sol
@@ -5,14 +5,33 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "hardhat/console.sol";
 
 /// @title NFT contract
 /// @author https://twitter.com/BossMcBara
-contract CardFactory is ERC721URIStorage, Ownable, ReentrancyGuard {
+contract CardFactory is
+    ERC721URIStorage,
+    Ownable,
+    ReentrancyGuard,
+    VRFConsumerBaseV2
+{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    address constant VRF_COORDINATOR =
+        0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+    VRFCoordinatorV2Interface COORDINATOR =
+        VRFCoordinatorV2Interface(VRF_COORDINATOR);
+    bytes32 constant KEY_HASH =
+        0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    uint32 constant CALL_BACK_GAS_LIMIT = 100000;
+    uint64 constant SUBSCRIPTION_ID = 206;
+    uint16 constant REQUEST_CONFIRMATIONS = 3;
+    uint256 requestId;
     address public cardMarketplaceAddress;
+    uint256[] public randomWords;
+    address[] public addressesEligibleForRewards;
     mapping(address => uint256) public rewardEligibilityMultiplier;
     mapping(uint256 => string) public nftRarity;
     // NFT's Id --> pack Id to which it belongs
@@ -26,6 +45,7 @@ contract CardFactory is ERC721URIStorage, Ownable, ReentrancyGuard {
     /// @param collectionSymbol The symbol for the NFT collection
     constructor(string memory collectionName, string memory collectionSymbol)
         ERC721(collectionName, collectionSymbol)
+        VRFConsumerBaseV2(VRF_COORDINATOR)
     {}
 
     /// @notice Sets the NFT marketplace address enabling default approval
@@ -136,5 +156,53 @@ contract CardFactory is ERC721URIStorage, Ownable, ReentrancyGuard {
             "verifyCompletePackOwnerShip: Sorry, you do not have a complete set :("
         );
         rewardEligibilityMultiplier[msg.sender] += 1;
+    }
+
+    /** @notice Only owner can call this function to distribute rewards amoungst
+    reward eligible accounts and who've been selected to win by a Chainlink VRF.
+    */
+    /// @param _rewardNftIds The NFT Ids that are selected as rewards
+    function distributeRewards(uint256[] memory _rewardNftIds)
+        public
+        onlyOwner
+    {
+        for (uint256 i = 0; i < randomWords.length; i++) {
+            transferFrom(
+                address(this),
+                addressesEligibleForRewards[randomWords[i]],
+                _rewardNftIds[i]
+            );
+        }
+    }
+
+    /** @notice Makes request to Chainlink VRF Oracle to get random number(s).
+    These random numbers will be used to select individual NFT's at random as
+    a reward for having a complete pack of NFTs.
+    */
+    /** @dev The amount of random numbers generated will be 1 num for every 5 accounts in
+    array that are eligible to win an NFT with an additional number for accounts 1 - 4. 
+    */
+    function requestRandomWords() external onlyOwner {
+        uint32 numWords = uint32(addressesEligibleForRewards.length) / 5 + 1;
+        requestId = COORDINATOR.requestRandomWords(
+            KEY_HASH,
+            SUBSCRIPTION_ID,
+            REQUEST_CONFIRMATIONS,
+            CALL_BACK_GAS_LIMIT,
+            numWords
+        );
+    }
+
+    /// @notice Fulfills randomness and assigns storage array to returned random numbers
+    function fulfillRandomWords(uint256, uint256[] memory _randomWords)
+        internal
+        override
+    {
+        for (uint256 i = 0; i < _randomWords.length; i++) {
+            _randomWords[i] =
+                randomWords[i] %
+                addressesEligibleForRewards.length;
+        }
+        randomWords = _randomWords;
     }
 }
