53198fe13f6052c491f576ee77c48ad7387850c3
McManOfTheLand <forrestchew@gmail.com>
2022-05-13 17:16:57 -0700

    Created script to automatically mint 1000 nfts

---
+++ scripts/factory-scripts/create-nft-collection-with-packs.js
@@ -0,0 +1,81 @@
+const { getCardFactoryInstance, marketplaceAddress } = require('../utils.js');
+
+const IRON_URI =
+  'https://gateway.pinata.cloud/ipfs/QmVo4Bv31zLRvFiAXL6Z8te1kmuu1gYNEnneyBb2FF2aNp';
+const GOLD_URI =
+  'https://gateway.pinata.cloud/ipfs/QmbJj1i9TRAhKRxNeMob7vL4zVE9iwqiySjr1RRNZPo6tC';
+const DIAMOND_URI =
+  'https://gateway.pinata.cloud/ipfs/Qma2NZzL1XaZEaGK86kxt32TvPLD9tQ5ZPCFGRBJou4KHf';
+
+// Naive implementation of weighting the rarities of NFTs getting minted
+const nftMetaData = [
+  IRON_URI,
+  IRON_URI,
+  IRON_URI,
+  IRON_URI,
+  IRON_URI,
+  IRON_URI,
+  GOLD_URI,
+  GOLD_URI,
+  GOLD_URI,
+  DIAMOND_URI,
+  DIAMOND_URI,
+];
+
+const mintCollectionWithPacksAdmin = async () => {
+  const cardFactory = await getCardFactoryInstance();
+  const nftRarityAmounts = { Iron: 0, Gold: 0, Diamond: 0 };
+  // Will mint 1000 total NFTs. 200 * 5 = 1000
+  for (let packId = 1; packId <= 200; packId++) {
+    for (let nftId = 1; nftId <= 5; nftId++) {
+      let tokenUri = randomTokenUri();
+      let nftRarity;
+      if (tokenUri == IRON_URI) {
+        nftRarity = 'Iron';
+        nftRarityAmounts['Iron'] += 1;
+      } else if (tokenUri == GOLD_URI) {
+        nftRarity = 'Gold';
+        nftRarityAmounts['Gold'] += 1;
+      } else {
+        nftRarity = 'Diamond';
+        nftRarityAmounts['Diamond'] += 1;
+      }
+      await mint(tokenUri, nftRarity, packId, marketplaceAddress, cardFactory);
+      console.log('Iron amount:', nftRarityAmounts['Iron']);
+      console.log('Gold amount:', nftRarityAmounts['Gold']);
+      console.log('Diamond amount:', nftRarityAmounts['Diamond']);
+    }
+  }
+  console.log('Iron amount:', nftRarityAmounts['Iron']);
+  console.log('Gold amount:', nftRarityAmounts['Gold']);
+  console.log('Diamond amount:', nftRarityAmounts['Diamond']);
+};
+
+// Helper function
+const randomTokenUri = () => {
+  // Returns INT between 0 - 11. The number will be used as index to decide which metadata to mint
+  const randInt = Math.floor(Math.random() * 11);
+  return nftMetaData[randInt];
+};
+
+const mint = async (
+  tokenUri,
+  nftRarity,
+  packId,
+  CARD_MARKETPLACE_ADDR,
+  cardFactory
+) => {
+  await cardFactory.createNFTWithApprovalAdminPack(
+    tokenUri,
+    nftRarity,
+    packId,
+    CARD_MARKETPLACE_ADDR
+  );
+};
+
+mintCollectionWithPacksAdmin()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
