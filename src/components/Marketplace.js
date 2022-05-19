c0a6a9133c6736ddbfb8b60931d8cc77f8a38603
Forrest Chew <forrestchew@gmail.com>
2022-05-18 21:39:27 -0600

    .map() through Moralis DB

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,24 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useMoralisQuery } from 'react-moralis';
+import { Moralis } from 'moralis';
+import { getCardFactoryInstance } from './ComponentUtils';
+import IsLoading from './IsLoading';
 import Card from './Card';
 import ActionInfoSection from './ActionInfoSection';
 import '../styles/cards.css';
 
 const Marketplace = () => {
   const packImg =
-    'https://imgs.search.brave.com/mcwnWR2bgfV8YNVaO92L6QaZer57E4lEKdWuDlwDVZs/rs:fit:600:500:1/g:ce/aHR0cHM6Ly9kM2xw/NHhlZGJxYThhNS5j/bG91ZGZyb250Lm5l/dC9zMy9kaWdpdGFs/LWNvdWdhci1hc3Nl/dHMvbm93LzIwMTcv/MTIvMjAvMTUxMzcy/OTQ5MzQxM19Eb25h/bGR0cnVtcHJvYm90/LmpwZz93aWR0aD02/OTAmaGVpZ2h0PSZt/b2RlPWNyb3AmcXVh/bGl0eT03NQ';
+    'https://imgs.search.brave.com/6ZRXjdjAX4Nn79yp-klSMBB6kDisZdnk5B-kkKTHrjE/rs:fit:695:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4y/QnEzMUIwMjFkUHA3/S1FLdWVaaDVBSGFG/RCZwaWQ9QXBp';
+  const [nftPackListings, setNftPackListings] = useState([]);
+  const { data, isLoading } = useMoralisQuery('NewPackListing');
+
+  useEffect(() => {
+    try {
+      setNftPackListings(data);
+    } catch (error) {
+      alert('Could not load packs. Please refresh page');
+    }
+  }, [data]);
+  console.log(data);
   return (
     <>
-      <div className='marketplace-components'>
-        <div className='nft-packs-container'>
-          <Card packImg={packImg} />
-          <Card packImg={packImg} />
-          <Card packImg={packImg} />
-          <Card packImg={packImg} />
-          <Card packImg={packImg} />
+      {isLoading ? (
+        <IsLoading />
+      ) : (
+        <div className="marketplace-components">
+          <div className="nft-packs-container">
+            {nftPackListings.map((nftPackListing, index) => {
+              const { packListingId, packPrice } = nftPackListing.attributes;
+              console.log(packListingId);
+              return (
+                <Card
+                  key={index}
+                  packImg={packImg}
+                  nftPackListingId={packListingId}
+                  packPrice={Moralis.Units.FromWei(packPrice)}
+                />
+              );
+            })}
+          </div>
+          <div className="action-info-section">
+            <ActionInfoSection packImg={packImg} />
+          </div>
         </div>
-        <div className='action-info-section'>
-          <ActionInfoSection packImg={packImg} />
-        </div>
-      </div>
+      )}
     </>
   );
 };
