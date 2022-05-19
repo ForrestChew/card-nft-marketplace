0de193bd17ba8b29ce172e5fca2f3fa8e6ec0eea
Forrest Chew <forrestchew@gmail.com>
2022-05-18 21:38:38 -0600

    Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,24 @@
+import Button from './Button';
 import '../styles/cards.css';
 
-const Card = ({ packImg }) => {
+const Card = ({ packImg, nftPackListingId, packPrice, nftCount }) => {
   return (
     <>
-      <div className='nft-pack-container'>
+      <div className="nft-pack-container">
         <img
           src={packImg}
-          className='nft-pack-img'
-          height='210'
-          width='256'
+          className="nft-pack-img"
+          height="210"
+          width="256"
         ></img>
-        <div className='nft-title-info'>
+        <div className="nft-title-info">
           <p>Dipshits</p>
-          <p>Id: 1</p>
+          <p>Item Count:</p>
+          <p>Price: {packPrice} </p>
+          <p>Id: {nftPackListingId}</p>
+        </div>
+        <div className="pack-btn">
+          <Button btnName="Buy" />
         </div>
       </div>
     </>
