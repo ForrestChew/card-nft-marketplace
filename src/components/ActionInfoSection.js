2f2af1f86d541ba4f12613716e20e14b9057810f
Forrest Chew <forrestchew@gmail.com>
2022-05-20 16:42:24 -0600

    Utilizing moralis component to save time reinventing the wheel

--- src/components/ActionInfoSection.js
+++ src/components/ActionInfoSection.js
@@ -1,11 +1,16 @@
-import { useState, useContext, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import { useMoralis } from 'react-moralis';
-import { AppContext } from './Context';
+import { NFT } from 'web3uikit';
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
-import { factoryAddress, factoryAbi } from '../contract-info/factory-info';
-import IsLoading from './IsLoading';
-import '../styles/cards.css';
+import {
+  marketplaceAddress,
+  marketplaceAbi,
+} from '../contract-info/marketplace-info';
+import { factoryAddress } from '../contract-info/factory-info';
+import '../styles/marketplace.css';
 import '../styles/action-info-section.css';
+import '../styles/main.css';
+
 const ActionInfoSection = ({
   packName,
   packImg,
@@ -15,54 +20,38 @@ const ActionInfoSection = ({
   packSeller,
   nftIds,
 }) => {
-  console.log(nftIds);
-  const [activePack] = useContext(AppContext);
-  const [nftImgUrls, setNftImgUrls] = useState([]);
   const [currentImgIdx, setCurrentImgIdx] = useState(0);
-  const [isLoading, setIsloading] = useState(false);
 
   const { Moralis } = useMoralis();
-  const provider = Moralis.web3;
-  const ethers = Moralis.web3Library;
-  const cardFactory = new ethers.Contract(factoryAddress, factoryAbi, provider);
 
-  useEffect(() => {
-    // Adds NFT img URIs to array to be displayed
-    const getNftImgs = async () => {
-      setNftImgUrls([]);
-      nftIds.map(async (nftId) => {
-        setIsloading(true);
-        const uri = await cardFactory.tokenURI(nftId);
-        fetch(uri)
-          .then(async (response) => {
-            const resJson = await response.json();
-            setNftImgUrls((nftImgUrls) => [...nftImgUrls, resJson['image']]);
-          })
-          .then(() => {
-            setIsloading(false);
-          });
-      });
-    };
-    getNftImgs();
-  }, [activePack]);
+  const buyPack = async () => {
+    const provider = Moralis.web3;
+    const ethers = Moralis.web3Library;
+    const signer = provider.getSigner();
+    const cardMarketplace = new ethers.Contract(
+      marketplaceAddress,
+      marketplaceAbi,
+      signer
+    );
+    await cardMarketplace.buyNftPack(packListingId, packSeller, {
+      value: Moralis.Units.ETH('1'),
+    });
+  };
 
   const nextImg = () => {
     setCurrentImgIdx(
-      currentImgIdx === nftImgUrls.length - 1 ? 0 : currentImgIdx + 1
+      currentImgIdx === nftIds.length - 1 ? 0 : currentImgIdx + 1
     );
   };
   const prevImg = () => {
     setCurrentImgIdx(
-      currentImgIdx === 0 ? nftImgUrls.length - 1 : currentImgIdx - 1
+      currentImgIdx === 0 ? nftIds.length - 1 : currentImgIdx - 1
     );
   };
 
-  // console.log(nftImgUrls);
-
   return (
     <>
-      <div className="info-section-container">
-        <img src={packImg} width="25%"></img>
+      <div className="info-section-container action-info-sec">
         <div className="pack-info">
           {/* <span>{packName}</span> */}
           <span>Pack Id: {packListingId}</span>
@@ -70,22 +59,27 @@ const ActionInfoSection = ({
           <span>NFT Count: {nftCount}</span>
           <span>Seller: {packSeller}</span>
         </div>
-      </div>
-      <section className="pack-imgs">
-        <div className="slide-img">
-          <FaArrowRight className="next-img" onClick={() => nextImg()} />
-          <FaArrowLeft className="prev-img" onClick={() => prevImg()} />
-          {!isLoading ? (
-            <img
-              src={nftImgUrls[currentImgIdx]}
-              className="slide-img"
-              width="70%"
-            ></img>
-          ) : (
-            <IsLoading />
-          )}
+        <div className="carousel-container">
+          <div className="arrows">
+            <FaArrowLeft className="prev-arrow" onClick={() => prevImg()} />
+            <FaArrowRight className="next-arrow" onClick={() => nextImg()} />
+          </div>
+          {console.log(nftIds)}
+          {console.log(nftIds[currentImgIdx])}
+          <NFT
+            address={factoryAddress}
+            chain="mumbai"
+            tokenId={nftIds[currentImgIdx]}
+            fetchMetadata="true"
+            className="nft-img"
+          />
         </div>
-      </section>
+        <div className="footer">
+          <button className="buy-btn" onClick={() => buyPack()}>
+            Buy Pack
+          </button>
+        </div>
+      </div>
     </>
   );
 };
