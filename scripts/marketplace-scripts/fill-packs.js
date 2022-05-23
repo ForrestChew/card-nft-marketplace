c6862fd4ba7e4520542acff7552d4c0ad5620967
Forrest Chew <forrestchew@gmail.com>
2022-05-22 22:27:44 -0600

    Adjusted for new name parameter

--- scripts/marketplace-scripts/fill-packs.js
+++ scripts/marketplace-scripts/fill-packs.js
@@ -1,16 +1,14 @@
-/*
-This script requires user to have deployed NFTs with the NFT card factory
-*/
 const { ethers } = require('hardhat');
 const { getCardMarketplaceInstance } = require('../utils.js');
 
 const ONE_ETHER = ethers.utils.parseEther('1');
 
-const listPack = async (tokenIdsToMint) => {
+const listPack = async (tokenIdsToMint, name) => {
   const cardMarketplace = await getCardMarketplaceInstance();
   const listPackReceipt = await cardMarketplace.listPack(
     ONE_ETHER,
-    tokenIdsToMint
+    tokenIdsToMint,
+    name
   );
   console.log('-'.padEnd(42, '-'));
   console.log(listPackReceipt);
@@ -18,7 +16,7 @@ const listPack = async (tokenIdsToMint) => {
   console.log('Pack Minted');
 };
 
-listPack([27, 28, 29, 30])
+listPack([28, 29, 30], 'Test_7')
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
