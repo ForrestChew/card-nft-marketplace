79f5ece7b988dbc2a45bc9019245877554699c9f
Forrest Chew <forrestchew@gmail.com>
2022-05-23 20:30:23 -0600

    Smaller height

--- src/components/Card.js
+++ src/components/Card.js
@@ -31,21 +31,24 @@ const Card = ({
           <img
             src={packImg}
             className="nft-pack-img"
-            height="210"
+            height="196"
             width="256"
           ></img>
         ) : (
           <img
-            height="210px"
+            height="196px"
             width="256px"
             style={{ visibility: 'hidden' }}
           ></img>
         )}
         <div className="below-img">
           <div className="nft-title-info">
-            <span className="info-text">Name: {packName}</span>
-            <span className="info-text">Price: {packPrice} Matic</span>
-            <span className="info-text">Ids: {nftIds}</span>
+            <span className="info-text">{packName}</span>
+            <span className="info-text">{packPrice} Matic</span>
+            <span className="info-text">{packListingId}</span>
+            {/* Labled here instead of component that passes props in order to set
+            NFT IDs in global context */}
+            <span className="info-text">{`NFT IDs: ${nftIds}`}</span>
           </div>
           <div className="pack-btn">
             <Button btnName="Details" onClick={() => displayActivePack()} />
